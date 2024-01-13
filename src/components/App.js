67abca87797528593c4b46085bc1a9f886c25e84
shahbaj khan <130038256+beingshahbaj@users.noreply.github.com>
2024-01-13 11:15:31 +0000

    complited

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name , setName] = useState("");
+   function handel(e){
+    let text ="hello" + " "  + (e.target.value) + " " +  "!";
+
+    setName(text)
+   }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>Enter your name</h1>
+       <input type="text" onChange={handel}></input>
+        <p>{name}</p>
     </div>
   )
 }
